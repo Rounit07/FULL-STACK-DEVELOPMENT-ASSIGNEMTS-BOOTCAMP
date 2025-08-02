d194f3b4c1d03ec1ea7ecca1a02be7c11d0f8e3f
Rounit07 <140837690+Rounit07@users.noreply.github.com>
2025-08-02 23:42:20 +0530

    Add files via upload

---
+++ ASSIGNMENT-15(WEATHER APP)/my-app/src/components/WeatherApp.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+let api = '74db55464cddd59d50b4e049d0adc1b3'
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Weather App</h1>
+
+      
+    </div>
+  )
+}
